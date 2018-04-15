62da8004899529d2706c83e2f9604b823956b78a
nikulpatel156 <38387697+nikulpatel156@users.noreply.github.com>
2018-04-15 18:27:26 +0100

    Connected to site to scrape & retrieved the page as a document.

--- src/sburys/scraper/SainsburysScraper.java
+++ src/sburys/scraper/SainsburysScraper.java
@@ -5,19 +5,53 @@
  */
 package sburys.scraper;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.jsoup.nodes.Document;
+import sburys.model.ProductFile;
+import sburys.scraper.util.SiteScraper;
+import sburys.scraper.util.impl.SiteScraperImpl;
+
 /**
  *
  * @author NIKUL PATEL
  */
 public class SainsburysScraper
 {
+    
+    private final String MAIN_SITE_URL = "https://jsainsburyplc.github.io/serverside-test/site/www.sainsburys.co.uk/webapp/wcs/stores/servlet/gb/groceries/berries-cherries-currants6039.html";
+    
+    private List<ProductFile> productFileList = new ArrayList<ProductFile>();
+    SiteScraper scraper;
+    
+    public SainsburysScraper()
+    {
+        scraper = new SiteScraperImpl();
+        
+        
+        Document mainSiteDocument = null;
+        try
+        {
+            mainSiteDocument = scraper.getSiteAsDocument(MAIN_SITE_URL);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(SainsburysScraper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        System.out.println(mainSiteDocument.hasText());
+        
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        SainsburysScraper ss = new SainsburysScraper();
     }
     
 }
